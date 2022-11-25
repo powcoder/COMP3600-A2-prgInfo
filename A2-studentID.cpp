63ea32f40314883038bac0b2a9ec2dffe1a8d9ff
powcoder <powcoder@163.com>
2022-11-25 15:23:52 +0800

    COMP3600 A2 prgInfo

---
+++ A2-studentID.cpp
@@ -0,0 +1,37 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#include <iostream>
+#include <fstream>
+// Add additional libraries you need here
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Please supply the name of the input file\n";
+    } 
+    else { 
+		// Read input
+		ifstream inFile(argv[1]);
+		int n, x, y;
+		inFile >> n;
+		for (int i = 0; i < n; i++) {
+			inFile  >> x >> y;
+			// Store x and y data
+		}
+		inFile.close();  		
+
+		int xh, yh;
+		// Place your algorithm here
+		// If you need to create a function, place the function above the main function
+		// The results of your algorithm should be placed in variable xh and yh
+		
+		
+		// Print output
+        cout << xh << " " << yh << "\n";
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# COMP3600 A2 prgInfo
+# 加微信 powcoder
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
